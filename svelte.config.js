6e44df6626541170ac79a6aced406bde09093282
Dunn, Simeon <simedunn01@gmail.com>
2026-04-04 19:19:38 -0400

    cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { relative, sep } from "node:path";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
